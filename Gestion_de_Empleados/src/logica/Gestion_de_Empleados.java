e839e50e4bb9361fa6639486466aa9d0eb4db322
Rodrigo268 <rodrigoalamino26@gmail.com>
2025-06-05 17:46:24 -0300

    Login y Menú agregados.

---
+++ Gestion_de_Empleados/src/logica/Gestion_de_Empleados.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+
+import IGU.login;
+public class Gestion_de_Empleados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login nuevoLogin = new login();
+        nuevoLogin.setVisible(true);
+        nuevoLogin.setLocationRelativeTo(null);
+    }
+    
+}
