9b356612bcf2c38b86ed818710150437f23f697f
Julieta-Laspiur <julieta.laspiur771@gmail.com>
2025-06-17 17:23:04 -0300

    Add files via upload

---
+++ Gestion_de_Emplados/src/main/java/logica/Gestion_de_Emplados.java
@@ -0,0 +1,14 @@
+
+package logica;
+
+import IGU.login;
+
+public class Gestion_de_Emplados {
+
+    public static void main(String[] args) {
+        login nuevoLogin = new login();
+        nuevoLogin.setVisible(true);
+        nuevoLogin.setLocationRelativeTo(null);
+        
+    }
+}
